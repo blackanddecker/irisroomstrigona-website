467f91ebde4169e3cfd8735d601ae8c63ead2cae
blackanddecker <baggelisfoufikos@yahoo.com>
2026-03-13 19:09:04 +0200

    add seo

--- app.py
+++ app.py
@@ -18,5 +18,21 @@ def contact():
 def activities():
     return render_template('activities.html')
 
+# @app.route("/sitemap.xml")
+# def sitemap():
+#     pages = []
+#
+#     pages.append("https://irisroomstrigona.gr/")
+#     pages.append("https://irisroomstrigona.gr/rooms")
+#     pages.append("https://irisroomstrigona.gr/activities")
+#     pages.append("https://irisroomstrigona.gr/contact")
+#
+#     xml = render_template("sitemap.xml", pages=pages)
+#
+#     response = make_response(xml)
+#     response.headers["Content-Type"] = "application/xml"
+#
+#     return response
+
 if __name__ == '__main__':
     app.run(debug=True)
